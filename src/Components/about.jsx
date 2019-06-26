2800ca989423aa237572b51a682a0367b34bd8a0
chris13012 <christophecaillet@icloud.com>
2019-06-26 17:22:09 +0200

    about

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -3,8 +3,39 @@ import React, { Component } from "react";
 class About extends Component {
   state = {};
   render() {
-    return <h1>A propos de moi</h1>;
+    return (
+      <div style={styles.container}>
+        <header style={styles.header}>
+          <p style={styles.p}>
+            Salut. Je suis Christophe, un marseillais de 36 ans.
+          </p>
+          <p style={styles.p}>
+            Recemment diplomé en developpement Front-End, je suis à la recherche
+            de mon premier poste de developpeur !
+          </p>
+          <p style={styles.p}>Voici mon histoire ...</p>
+        </header>
+      </div>
+    );
   }
 }
+
+const styles = {
+  container: {
+    textAlign: "center",
+    fontFamily: "Open Sans, sans-serif"
+  },
+  header: {
+    fontSize: 25,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    margin: "25px 100px"
+  },
+  p: {
+    margin: 0,
+    padding: 0,
+    lineHeight: 1
+  }
+};
 
 export default About;
