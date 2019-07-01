fcf21fef5f5dce12619929afebbede4a7d74ae9b
chris13012 <christophecaillet@icloud.com>
2019-07-01 15:11:01 +0200

    center responsive

--- src/Components/homepage.jsx
+++ src/Components/homepage.jsx
@@ -3,7 +3,6 @@ import { Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 import { bounce } from "react-animations";
-import Background from "../images/2.jpg";
 
 const Bounce = styled.div`
   animation: 2s ${keyframes`${bounce}`} infinite;
@@ -14,18 +13,20 @@ class Homepage extends Component {
   render() {
     return (
       <div style={styles.container}>
-        <div style={styles.p}>
-          Bonjour, je suis{" "}
-          <Bounce style={styles.nom}>
-            <span>Christophe Caillet</span>
-          </Bounce>
+        <div style={styles.phrases}>
+          <div style={styles.p}>
+            Bonjour, je suis{" "}
+            <Bounce style={styles.nom}>
+              <span>Christophe Caillet</span>
+            </Bounce>
+          </div>
+          <p style={styles.p}>votre futur developpeur Web ?</p>
+          <Link to="/about">
+            <Button style={styles.button} color="primary" size="lg">
+              Voici mon travail
+            </Button>
+          </Link>
         </div>
-        <p style={styles.p}>votre futur developpeur Web ?</p>
-        <Link to="/about">
-          <Button style={styles.button} color="primary" size="lg">
-            Voici mon travail
-          </Button>
-        </Link>
       </div>
     );
   }
@@ -33,22 +34,17 @@ class Homepage extends Component {
 
 const styles = {
   container: {
-    backgroundImage: `url(${Background})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
     color: "white",
     fontSize: 40,
     fontFamily: "Open Sans, sans-serif",
     height: "100vh",
     minHeight: "100vh",
-    display: "flex",
+    display: "grid",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column"
+    textAlign: "center"
   },
   p: {
-    margin: 2,
     padding: 0,
     lineHeight: 1
   },
