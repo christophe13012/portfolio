89a2176a362b8c64586068bb539e0a63cbeafa3b
chris13012 <christophecaillet@icloud.com>
2019-08-22 11:20:05 +0200

    Updates

---
+++ precache-manifest.3f81f81e305e730b264accaa394698cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3981d42109a31274bf3346c7505790",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "294708a0e6ec40ba8cbc",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "86d563d1bc8cad4dea32",
+    "url": "/portfolio/static/css/main.3fe037ac.chunk.css"
+  },
+  {
+    "revision": "294708a0e6ec40ba8cbc",
+    "url": "/portfolio/static/js/2.ca2fda53.chunk.js"
+  },
+  {
+    "revision": "86d563d1bc8cad4dea32",
+    "url": "/portfolio/static/js/main.38be14e3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c9aa33b3744be8c509e9c7d5ab2c6724",
+    "url": "/portfolio/static/media/2.c9aa33b3.jpg"
+  }
+]);
