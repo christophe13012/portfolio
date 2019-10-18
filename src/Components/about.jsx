7c80cb64ad5a2f50fce3f01054dda948dad591b7
christophe13012 <christophecaillet@icloud.com>
2019-10-18 11:16:08 +0200

    correction ortho

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -32,24 +32,24 @@ class About extends Component {
                     sortie d'un bac ES, je me suis orienté dans la logistique
                     jusque occuper maintenant un poste de responsable d'équipe.
                     Mais il y a 3 ans j'ai découvert avec passion le
-                    developpement web pour un besoin professionnel, et c'est la
+                    développement web pour un besoin professionnel, et c'est la
                     révélation, je veux en faire mon métier.
                   </p>
                   <p>
                     J'ai donc suivi de nombreux cours notamment sur
                     Openclassrooms et décidé de m'orienter en Front.
                     Openclassrooms propose de suivre des parcours diplomants et
-                    je m'inscris en juillet 2018 en parallele de mon emploi au
+                    je m'inscris en juillet 2018 en parallèle de mon emploi au
                     parcours Développeur d'application - Frontend délivrant un
                     Diplôme RCNP niveau Bac+3/4, que j'ai validé en mars 2019.
                     j'ai décidé ensuite de me perfectionner durant quelques mois
-                    avant de checher activement un emploi dans ce domaine.
+                    avant de chercher activement un emploi dans ce domaine.
                   </p>
                   <p>
                     J'ai décidé de me spécialiser sur React, j'ai donc suivi des
                     cours et fait beaucoup de projets, vous pourrez voir les
                     plus aboutis dans la section Porfolio, et j'ai également
-                    fait quelques applications en React Native, étant passioné
+                    fait quelques applications en React Native, étant passionné
                     des produits Apple, c'est un bel aboutissement pour moi.
                   </p>
                   <p>Au plaisir de pouvoir discuter avec vous.</p>
